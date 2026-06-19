257dc115aa7f8d67d5cccdf86903278bc4297cc2
echo <ali.amt1388@gmail.com>
2026-06-20 00:53:17 +0330

    test(feat): add notes app admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Note, Comment, Like
 
-# Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'owner', 'is_public')
+
+    search_fields = ('title', 'owner')
+
+    ordering = ('created_at',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'user')
+
+    search_fields = ('owner',)
+
+    ordering = ('created_at',)
