8d665e96273e7dbda1ee2684be8501c9940c5685
echo <ali.amt1388@gmail.com>
2026-06-28 20:57:41 +0330

    fix(notes): rename widget to widgets in CommentForm

--- notes/forms.py
+++ notes/forms.py
@@ -11,6 +11,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-        widget = {
+        widgets = {
             'content': forms.Textarea(attrs={'rows': 2, 'placeholder': 'Write a comment...'}),
         }
