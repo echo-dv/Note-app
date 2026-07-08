479829ee5caabe6040804f2fb8e1aeca27e0179d
echo <ali.amt1388@gmail.com>
2026-07-08 04:31:07 +0330

    feat(health): add health check endpoint

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('notes/', include('notes.urls')),
+    path('health/', include('core.health.urls'))
 ]
