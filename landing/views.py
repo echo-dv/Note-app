4d5925bd1cc1f9ea3be396c6ee3c8a0e4d968d6b
echo <ali.amt1388@gmail.com>
2026-08-03 02:14:47 +0330

    fix(landing): redirect authenticated users from landing page to home page

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
 class LandingView(TemplateView):
     template_name = "landing/landing_page.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("accounts:home")
+        return super().dispatch(request, *args, **kwargs)
